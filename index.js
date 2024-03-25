b47f34310cbe3487e9ef00e1ca3c8648ce64da20
NB <nasrobegg@gmail.com>
2024-03-25 17:19:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Spotify","Made by NB"];
 
 
 let currentIndex = 0;
